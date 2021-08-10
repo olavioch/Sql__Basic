b462be6479a4eb1bad59b8cc56e5332e1f50b70d
olavo argemiro <olaviochampions@gmail.com>
2021-08-10 13:43:30 -0300

    primeiros comandos my sql

---
+++ pessoas.sql
@@ -0,0 +1,9 @@
+CREATE TABLE pessoas(
+id INT NOT NULL PRIMARY KEY AUTOINCREMENT,
+nome VARCHAR(30) NOT NULL,
+nascimento DATE,
+)
+INSERT INTO `pessoas` (nome, nascimento) VALUES ('olavo', '1996-10-26')
+INSERT INTO `pessoas` (nome, nascimento) VALUES ('alice', '2001-07-11')
+INSERT INTO `pessoas` (nome, nascimento) VALUES ('olavo', '2001-01-01')
+INSERT INTO `pessoas` (nome, nascimento) VALUES ('olavo', '2002-02-02')
